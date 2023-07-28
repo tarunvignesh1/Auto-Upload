0050f91bcf5c247879597e7ab70ec3b4adf801e5
Tarun Vignesh Selvabalaji <tarunvignesh1@gmail.com>
2023-07-28 17:46:42 +0530

    adding files

--- Py-video-creator/yt-video-dnlder.py
+++ Py-video-creator/yt-video-dnlder.py
@@ -1,49 +1,46 @@
-import os
 import sys
+import os
 import requests
+from bs4 import BeautifulSoup
 from pytube import YouTube
-import re
 
-def search_youtube_videos(search_query, num_videos):
-    search_url = f"https://www.youtube.com/results"
-    response = requests.get(search_url, params={"search_query": search_query})
-    video_ids = re.findall(r"watch\?v=(\S{11})", response.text)
+def get_video_links(search_string, count):
+    base_url = f"https://www.youtube.com/results?search_query={search_string}"
+    response = requests.get(base_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    video_links = []
 
-    video_urls = [f"https://www.youtube.com/watch?v={video_id}" for video_id in video_ids]
+    for link in soup.select(".yt-uix-tile-link"):
+        video_links.append("https://www.youtube.com" + link["href"])
 
-    filtered_video_urls = []
-    for video_url in video_urls:
-        response = requests.get(video_url)
-        if "LIVE_STREAM_OFFLINE" not in response.text:
-            filtered_video_urls.append(video_url)
+        if len(video_links) == count:
+            break
 
-    return filtered_video_urls[:num_videos]
+    return video_links
 
-def download_videos(video_urls):
-    os.makedirs("downloaded_videos", exist_ok=True)
+def download_videos(video_links):
+    if not os.path.exists("py-video-creator"):
+        os.makedirs("py-video-creator")
 
-    for video_url in video_urls:
+    for link in video_links:
         try:
-            video = YouTube(video_url)
-            video_stream = video.streams.filter(file_extension='mp4').first()
-            if video_stream is not None:
-                print(f"Downloading video: {video.title}")
-                file_path = video_stream.download("downloaded_videos")
-                print(f"Downloaded: {video.title}")
-
+            yt = YouTube(link)
+            video = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first()
+            if video:
+                print(f"Downloading: {yt.title}")
+                video.download(output_path="downloaded_vidoes")
+            else:
+                print(f"No high-resolution video available for: {yt.title}")
         except Exception as e:
-            print(f"Error downloading video: {e}")
+            print(f"Error while downloading: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python youtube_search_and_download.py <SEARCH_TERM> <NUM_VIDEOS>")
+        print("Usage: python video_downloader.py <search_string> <count>")
         sys.exit(1)
 
-    search_query = sys.argv[1] + " japanese lofi"
-    num_videos = int(sys.argv[2])
+    search_string = sys.argv[1]
+    count = int(sys.argv[2])
 
-    video_urls = search_youtube_videos(search_query, num_videos)
-    if not video_urls:
-        print("No videos found for the given search term.")
-    else:
-        download_videos(video_urls)
+    video_links = get_video_links(search_string, count)
+    download_videos(video_links)
