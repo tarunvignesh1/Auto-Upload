71ba42d9da6f471e15f53abd4fe7dde78c191c33
Tarun Vignesh Selvabalaji <tarunvignesh1@gmail.com>
2023-07-28 19:29:20 +0530

    renaming

--- Py-video-creator/playlist-scraper.py
+++ Py-video-creator/playlist-scraper.py
@@ -1,14 +1,17 @@
 from pytube import Playlist, YouTube
 from sys import argv
 
-def download_videos_from_playlist(playlist_url, output_path):
+def download_videos_from_playlist(playlist_url, output_path,vid_count):
     try:
         playlist = Playlist(playlist_url)
-        
+        count = 0
         for video_url in playlist.video_urls:
             yt = YouTube(video_url)
             video_stream = yt.streams.filter(file_extension='mp4', res="720p").first()
+            count = count + 1
             if video_stream is not None:
+                if count >= vid_count:
+                    break
                 print(f"Downloading: {yt.title}")
                 video_stream.download(output_path)
             else:
@@ -19,5 +22,6 @@ def download_videos_from_playlist(playlist_url, output_path):
 
 if __name__ == "__main__":
     playlist_url = argv[1]
+    vide_count = int(argv[2])
     output_path = "downloaded_videos"
-    download_videos_from_playlist(playlist_url, output_path)
+    download_videos_from_playlist(playlist_url, output_path,vide_count)
